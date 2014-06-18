609b8cb8a3305dbfe2d53f04346567a4b167b6f3
fede2001 <fede2001@hotmail.com>
2014-06-17 21:54:59 -0500

    agrego desarrolloWeb y metodo ciclo tres meses

---
+++ desarrolloWeb.py
@@ -0,0 +1,7 @@
+
+# verificaciones lógicas para aceptar cupos 
+if web cursos revisan tiempo disponible
+edad 
+horario
+pre-requisitos
+completado exitosamente?
--- notas.py
+++ notas.py
@@ -3,15 +3,5 @@ prosessingJS
 exec fede manda d3 a johnny
 exec esteban mira el codigo
 
-if cursos revisan tiempo disponible
-edad 
-horario
-pre-requisitos
-completado exitosamente?
-
-javascript
-Objeto esun contenedor id y valor
-controlador se define con acciones:  acciones ejecutar modelo
-
 # ensambles fractales, 
 nosotros punchis punchis, efectos, ruido.
