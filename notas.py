acca1d9f1dea6b8212722d618234322aa7f845fc
fede2001 <fede2001@hotmail.com>
2014-07-31 17:09:34 -0500

    actualizao notas

--- notas.py
+++ notas.py
@@ -1,3 +1,185 @@
+
+
+
+
+# Notas
+25 julio 
+bajar el tempo cuando no suena bien
+si vienen nuevos uno experto le ayuda a uno nuevo
+los mas expertos pueden coger instrumentos mas sofisticados
+
+muuy bien manejado lo de los niños
+se puede invitar a la gente que esta afuera?
+
+poner un letrero en la puerta PUEDE ENTRAR
+
+solo usar instrumentos melódicos para lo de acordes, luego para ensamblar si con todos con percu 
+
+ellos pueden sintonizar a los chicos
+no es necesario explicar mucho por ejemplos filtros
+
+no hay estructura los ejercicios no estan planeados, los instrumentos se desafinan
+
+----
+
+
+
+
+
+2 miércoles del mes DataPotata
+
+ultimo viernes de cada mes algoritmo bailables
+
+jueves 9:30 clase con brolin
+
+
+
+Lunes (4 a 8) Belen
+
+Esteban 
+Miercoles en la mañana Cuerdas
+Martes en la mañana  Programación para audio (sitio por definir)
+Viernes en la tarde Guayabbal (tarde) 
+
+Pedro 
+Lunes belen en la tarde
+Martes 12 de octubre en la tarde
+Viernes Guayabal en la tarde
+ 
+Pipe: 
+Lunes Belen 4 a 8,
+Miércoles Los Colores (mañana)
+Viernes Guayabal 4 a 8
+
+Gnomo
+Lunes Belen en la tarde
+Martes 12 de Octubre tarde
+Viernes Guayabal en la tarde
+
+Daniel Luna
+Miercoles Alcazares en la tarde
+Lunes en Belen
+Viernes en Guayabal
+
+Johnny
+
+Brolin
+
+
+
+
+brolin, programación música matemática, lutier 
+
+Fede
+8 proyectos tiburo
+4 comunicaciones
+8 acompañamiento clases 
+4 informes
+
+Pipe
+4 horas de clase
+8 horas de preparación de esas clases
+8 horas proyecto tiburón
+4 instrumento mínimo.
+
+Johnny
+12 proyecto tiburon
+8 brolin
+4 clases 
+4 ayuda a proyecto tib de Nomo
+
+Pedro
+4 clase 
+8 preparar
+8 pt johnny
+4 proyecto tiburon
+
+Esteban 
+4 horas al instrumento mínimo
+4 horas a proy tiburón pedro
+8 horas a brolin
+8 su propio proyecto tiburón
+
+Dani
+12 proyecto tiburon 
+8 soporte electrónico laboratorio de todo el equipo, soldadura, electrónica
+4 destresa en el ruido y sus aparatos.
+
+Nomo
+8 horas asistiendo a pipe a pedro
+8 proyecto tiburon
+4 documenta festivales
+4 letras con camilo suarez
+
+
+
+## Pedro
+No mencionar cantoalegre
+solo medellín vive la música
+vamos a enfocarnos en la música -- ellos son los que dicen si es divertida o no
+muy bien lo del experimento de todos aportando.
+
+## asistencia
+Vino Juan Jose
+y otro chico
+
+brutal el ensamble
+
+## Johnny 
+no entrar en detalles de usb, ni del software
+de una a la música
+
+pagina web: 
+view de consulta de edades
+colegios 
+sexo nodo y rango de edad
+sistema de asistencia
+medir la desersión
+inscritos vs. empezaron
+no inscrintos vs. empezaron
+
+empezaron vs. desersión
+
+desersión por materia
+
+
+mostrarle a la alcaldía pueda tener costo niño
+
+el profesor debe poder bajar la lista de asistencia de la página
+
+super admin
+administrador (estrategia, cantidades)
+
+ 
+
+
+la idea de instrumento minimo es que puedan participar muchos no alguien en un computadors
+
+hay dos tipos de actividades
+  actividad de ensamble 
+    practica
+    o participan todos
+
+  asignatura o área espcífica
+
+la Red
+en semana 2 horas estudio
+sabado 4 horas ensamble  
+
+2 a 4 profesores simultáneos
+
+lo que queremos enseñar lo traducimos a un ensamble, ese ensamble lo partimos en secciones para trabajar por grupos, la clase 
+
+repertorios 
+inicial
+intermedio
+avanzado
+mixtos para un grupo muy diverso
+
+
+herramienta para compartir los ensambles, grabacion, codificación etc.
+
+
 notas
 prosessingJS
 exec fede manda d3 a johnny
