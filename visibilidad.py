a9ca4b2da59f03a2c716918cca4e351b88151750
fede2001 <fede2001@hotmail.com>
2015-02-14 11:12:06 -0500

    visibilidad

---
+++ visibilidad.py
@@ -0,0 +1,17 @@
+¿Cómo aumentar la visibilidad de los procesos formativos en que participamos de manera coherente con nuestros principios?
+
+- Becas y casos
+- Embellecer el entorno
+- No hay intermediario, no hay powerpoint, no es un taller, es un talento allí para asombrar y conversar sobre las inquietudes de los asistentes o simplemente estar allí para asombrarse con su pasión y el porqué es una actividad grandiosa.
+
+- premios? no es nuestro estilo
+- manejar la "Identidad visual temporal"
+- ser claros con los propósitos
+- Project Awarnes System (PAS) aumenta la noción de los proyctos y los participantes más allá de las fronteras de la organización para aumentar comunicación, colaboración, y reuso de los resultados del proyecto.
+http://link.springer.com/chapter/10.1007%2F978-3-642-39371-6_19#page-1
+
+- Como desembocamos procesos que hicimos en servicios permanentes? 
+  - Letras de hiphop
+  - bases de datos
+  - wiki
+  
